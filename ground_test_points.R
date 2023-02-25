df93c1c7baba5c8ab2313d036ad11bf37b336c30
Liam Berigan <liamaberigan@gmail.com>
2023-02-25 08:23:09 -0500

    First working model
    Two state, has issues distinguishing between stopovers and mortality. Consider a 3 state structure.

--- ground_test_points.R
+++ ground_test_points.R
@@ -9,3 +9,12 @@ ground_test %>%
   filter(ID == 215688) %>% 
   sf::st_as_sf(coords = c("x", "y"), crs = 4326) %>% 
   mapview::mapview()
+
+amwo_hmm %>% 
+  group_by(test) %>% 
+  summarise(mn_step = mean(step, na.rm = TRUE),
+            sd_step = sd(step, na.rm = TRUE),
+            mn_angle = mean(angle, na.rm = TRUE),
+            sd_angle = sd(angle, na.rm = TRUE),
+            mn_dist = mean(mean_dist_5),
+            sd_dist = sd(mean_dist_5))
